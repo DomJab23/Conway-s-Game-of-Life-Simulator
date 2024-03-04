bc8782f1b0be729b47a656a675d8e52f46e6b72a
DomJab23 <dojab23@studnet.sdu.dk>
2024-03-04 12:40:32 +0100

    Sceleton

---
+++ ConwaysGameofLifeSimulator/ICell.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Predator.Desktop.myprogram.Conway-s-Game-of-Life-Simulator
+{
+    public interface ICell
+    {
+        
+    }
+}
---
+++ TestConwaysGameofLifeSimulator/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace TestConwaysGameofLifeSimulator;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}

894ca36dbe87828e1d01fdb4ff240b10eec4a98f
DomJab23 <dojab23@studnet.sdu.dk>
2024-03-04 12:43:27 +0100

    ICell new

--- ConwaysGameofLifeSimulator/ICell.cs
+++ ConwaysGameofLifeSimulator/ICell.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Predator.Desktop.myprogram.Conway-s-Game-of-Life-Simulator
+namespace ConwaysGameofLifeSimulator
 {
     public interface ICell
     {
